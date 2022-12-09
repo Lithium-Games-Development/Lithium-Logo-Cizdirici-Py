16c991eb497a370deafa75ee195f9d8bafdae0b0
ErenKeles09 <09erenkeles09@gmail.com>
2022-12-09 23:11:44 +0300

    dairealandeneme

--- deneme.py
+++ deneme.py
@@ -1,8 +1,11 @@
 import math 
 def karekok(x):
     print(math.sqrt(x))
+def dairealan(yaricap):
+    alan=math.pi(yaricap**2)
+    return alan
 
 
 a=int(input("sayı giriniz:"))
-
+print(dairealan(5))
 
