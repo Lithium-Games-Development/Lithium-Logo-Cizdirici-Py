e7b9a5f687580b055e8c5d2b77c4d3f50b4c6b5d
Eren Keleş <101580270+ErenKeles0@users.noreply.github.com>
2022-12-10 13:27:14 +0300

    Update deneme.py

--- deneme.py
+++ deneme.py
@@ -2,7 +2,7 @@ import math
 def karekok(x):
     kok=math.sqrt(x)
     return kok
-
+asdasd
 def dairealan(yaricap):
     alan=math.pi*(yaricap**2)
     return alan
@@ -11,27 +11,3 @@ b=int(input("daire alanı alınacak yarıçap giriniz:"))
 print(karekok(a))
 print(dairealan(b))
 asdasdasd
-
-
-
-<<<<<<< HEAD
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-asdasdasdasd
-=======
-asdasdasdasd
->>>>>>> b06d075b5f0f3936268173e3839c2e5ebacecb53
