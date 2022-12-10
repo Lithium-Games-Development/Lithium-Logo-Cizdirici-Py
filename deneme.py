3ee4296d328433907dbf83aff377cd57727c2c12
ErenKeles09 <09erenkeles09@gmail.com>
2022-12-10 13:36:34 +0300

    LİTHİUM LOGO ÇİZDİRİCİ EKLENDİ

--- deneme.py
+++ deneme.py
@@ -1,14 +1,73 @@
-import math 
-def karekok(x):
-    kok=math.sqrt(x)
-    return kok
+import turtle
+#LİTHİUM LOGO ÇİZDİRİCİ
+lithiumarkaplan = turtle.Screen()
+lithiumcizici = turtle.Turtle()
+lithiumarkaplan.setup(width=1920,height=1080)
+lithiumarkaplan.bgcolor("red")
+lithiumarkaplan.title("LİTHİUM")
+lithiumcizici.color("white")
+lithiumcizici.speed(7.5)
+lithiumcizici.pensize(20)
+lithiumcizici.penup()
+lithiumcizici.goto(0,100)
+for i in range(0,4):
+    lithiumcizici.pendown()
+    lithiumcizici.left(90)
+    lithiumcizici.forward(250)
+    lithiumcizici.goto(0,100)
+lithiumcizici.left(45)
+for i in range(0,4):
+    lithiumcizici.pendown()
+    lithiumcizici.left(90)
+    lithiumcizici.forward(200)
+    lithiumcizici.goto(0,100)
+    lithiumcizici.goto(0,100)
+lithiumcizici.left(22.5)
+for i in range(0,8):
+    lithiumcizici.left(45)
+    lithiumcizici.forward(150)
+    lithiumcizici.goto(0,100)
 
-def dairealan(yaricap):
-    alan=math.pi*(yaricap**2)
-    return alan
-a=int(input("karekök alınacak sayı giriniz:"))
-b=int(input("daire alanı alınacak yarıçap giriniz:"))
-print(karekok(a))
-print(dairealan(b))
+lithiumcizici.goto(0,100)
+lithiumcizici.left(67.5)
+for i in range(0,4):
+    lithiumcizici.penup()
+    lithiumcizici.left(90)
+    lithiumcizici.forward(250)
+    lithiumcizici.pendown()
+    lithiumcizici.forward(25)
+    lithiumcizici.penup()
+    lithiumcizici.goto(0,100)
 
-selamsdasdasd
+lithiumcizici.right(67.5)
+lithiumcizici.penup()
+lithiumcizici.right(67.5)
+lithiumcizici.goto(0,-100)
+lithiumcizici.pendown()
+lithiumcizici.circle(200)
+
+
+lithiumcizici.penup()
+lithiumcizici.goto(0,90)
+lithiumcizici.pendown()
+lithiumcizici.color("red")
+lithiumcizici.pensize(25)
+lithiumcizici.circle(10)
+lithiumcizici.color("white")
+lithiumcizici.pensize(10)
+lithiumcizici.penup()
+lithiumcizici.goto(-60,-150)
+lithiumcizici.pendown()
+lithiumcizici.write("20", font=("Verdana", 20,"bold"))
+lithiumcizici.penup()
+lithiumcizici.goto(25,-150)
+lithiumcizici.pendown()
+lithiumcizici.write("23", font=("Verdana", 20,"bold"))
+lithiumcizici.penup()
+lithiumcizici.goto(-190,-300)
+lithiumcizici.pendown()
+lithiumcizici.write("LİTHİUM", font=("Verdana", 72))
+
+lithiumcizici.penup()
+
+lithiumcizici.goto(999,999)
